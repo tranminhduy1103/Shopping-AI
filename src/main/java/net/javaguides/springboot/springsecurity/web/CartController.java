5739ce53c271e83dfab8734b2eda5d2ac5e52fb6
LLHill <hieupro.st@gmail.com>
2021-05-09 21:32:35 +0700

    Revert: CartManagement

--- src/main/java/net/javaguides/springboot/springsecurity/web/CartController.java
+++ src/main/java/net/javaguides/springboot/springsecurity/web/CartController.java
@@ -1,11 +1,16 @@
 package net.javaguides.springboot.springsecurity.web;
 
+import net.javaguides.springboot.springsecurity.Entity.CartItem;
+import net.javaguides.springboot.springsecurity.Entity.User;
 import net.javaguides.springboot.springsecurity.service.CartServices;
 import net.javaguides.springboot.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CartController {
@@ -18,12 +23,8 @@ public class CartController {
 
     @GetMapping("/cart")
     public String showShoppingCart(Model model){
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        List<CartItem> cartItems = cartServices.listCartItems(user);
-//
-//        model.addAttribute("cartItems",cartItems);
-//        model.addAttribute("pageTitle","Shopping cart");
-        String[] cartItems = new String[] {"Pc","Laptop","earphone"};
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<CartItem> cartItems = cartServices.listCartItems(user);
         model.addAttribute("cartItems", cartItems);
         return "Cart_page";
     }
